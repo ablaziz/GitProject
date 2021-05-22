aa015d64bfc701f1932c64aaeb9d22d578f6bbe7
Aziz Niada <niadaaaziz@yahoo.fr>
2021-05-22 12:24:18 -0400

    wok done

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,6 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("making changes");
-        //adding a comment
+        //adding a commen;
+        //word
     }
 }
